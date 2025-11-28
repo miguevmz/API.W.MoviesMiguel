f5bf5cdc6152661b6f4dafad69c72afef024eb39
Thor <Thor@ING013>
2025-11-28 18:28:09 -0500

    Add project files.

---
+++ API.W.Movies/DAL/Models/Category.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.W.Movies.DAL.Models
+{
+    public class Category : AuditBase
+    {
+        [Required] //Este data annotation indica que el campo es obligatorio
+        [Display(Name ="Nombre de la categoria")] //Me sirve para personalizar el nombre que se muestra en las vistas o mesnajes de error
+        public string Name { get; set; }
+    }
+}
